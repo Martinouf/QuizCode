7af201025f3b7760ace54277e18276ee14c3dad0
Martin Binant <martinbinant.pro@gmail.com>
2024-01-25 09:38:48 +0100

    création des pages

---
+++ lib/page/flutter_quiz_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterQuizPage extends StatelessWidget {
+  const FlutterQuizPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/page/inscription_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InscriptionPage extends StatelessWidget {
+  const InscriptionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
